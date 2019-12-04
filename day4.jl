20f4f5fd46d7e13e9ce5f6a3653436ff798d38fb
Kari Silvennoinen <kari@kalifi.org>
2019-12-04 08:57:06 +0200

    day 4 part 2

--- day4.jl
+++ day4.jl
@@ -1,12 +1,17 @@
 function password_enumerator(s, e)::Int
-    valid = []
+    valid = 0
     for i = s:e
         if length(unique(digits(i))) <= 5 && sum(reverse(digits(i))[2:end] .>= reverse(digits(i))[1:end-1]) == 5
-            println(i)
-            push!(valid, i)
+            for j in unique(digits(i))
+                if sum(x->x==j, digits(i)) == 2
+                    # println(i)
+                    valid = valid + 1
+                    break
+                end
+            end
         end
     end
-    return length(valid)
+    return valid
 end
 
 println(password_enumerator(236491,713787))
