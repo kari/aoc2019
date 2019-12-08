47f94738bfb1cb217125bd37c62f1747b61ff3e6
Kari Silvennoinen <kari@kalifi.org>
2019-12-08 09:54:33 +0200

    day 8 part 2

--- day8.jl
+++ day8.jl
@@ -2,6 +2,8 @@ image = open("password.sif") do f
     reshape([parse(Int, s) for s in readlines(f)[1]], 25, 6, :)
 end
 
+# image = reshape([parse(Int, s) for s in "0222112222120000"], 2, 2, :)
+
 function checksum(image::Array{Int,3})::Int
     min = size(image, 1)*size(image, 2)
     prod = 0
@@ -18,4 +20,27 @@ function checksum(image::Array{Int,3})::Int
     return prod
 end
 
+function visible_pixel(pixels::Array{Int,1})::Int
+    for p in pixels
+        # println(p)
+        if p == 0 || p == 1
+            return p
+        end        
+    end
+    return -1
+end
+
+function parse_image(image::Array{Int,3})::Array{Int,2}
+    res = zeros(Int, size(image, 1), size(image, 2))
+    for x in 1:size(image, 1), y in 1:size(image,2)
+        # println(image[x, y, :])
+        res[x, y] = visible_pixel(image[x, y, :])
+    end
+    for row in 1:size(res, 2)
+        println(replace(join(res[:,row]), "0" => " "))
+    end
+    return res
+end
+
 println(checksum(image))
+parse_image(image)
