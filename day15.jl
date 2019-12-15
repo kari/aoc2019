d47b3a3f0992754f6ea9762f739ae1034c3ec053
Kari Silvennoinen <kari@kalifi.org>
2019-12-15 20:34:04 +0200

    day 15 part 2

--- day15.jl
+++ day15.jl
@@ -1,4 +1,4 @@
-using Random
+using Serialization
 
 function deref(pointer::Int, program::Array{Int,1}, mode::Int, base::Int)::Int
     if mode == 0
@@ -113,7 +113,7 @@ end
 function draw_map(map::Array{Int, 2}, min::Vector{Int}, max::Vector{Int})
     map = string.(map)
     for row in eachrow(map[min[2]-1:max[2]+1,:])
-        println(replace(replace(replace(replace(replace(join(row[min[1]-1:max[1]+1]), "0" => " "), "1" => "."), "2" => "#"), "4" => "S"), "3" => "F"))
+        println(replace(replace(replace(replace(replace(replace(join(row[min[1]-1:max[1]+1]), "0" => " "), "1" => "."), "2" => "#"), "4" => "S"), "3" => "F"), "7" => "O"))
     end
 end
 
@@ -126,6 +126,11 @@ function draw_map(map::Array{Int, 2}, robot::Vector{Int}, target::Vector{Int}, m
     end
 end
 
+function draw_map(map::Array{Int, 2})
+    min = findfirst(x -> x != 0, map)
+    max = findlast(x -> x != 0, map)
+    draw_map(map, [min[2], min[1]], [max[2], max[1]])
+end
 
 function draw_map(map::Array{Int, 2}, robot::Vector{Int}, direction::Int, queue::Vector{Vector{Int}})
     map = deepcopy(map)
@@ -404,19 +409,82 @@ function process_output(in::Channel, out::Channel)
 
     end
     # println("Mapped in ", counter) # 2444
-    draw_map(room, min, max)
-    target = findfirst(x -> x == 3, room)
-
-    return length(route([500,500], [target[2], target[1]], room))
+    return room
 end
 
-program = open("droid.csv") do f
-    parse.(Int, split(readlines(f)[1], ","))
+function fill(arr, start)
+    min = findfirst(x -> x != 0, arr)
+    max = findlast(x -> x != 0, arr)
+
+    i = 0
+    arr = map(x -> x == 2 ? -99 : x == 0 ? -98 : -1, arr) # block walls
+    
+    # 1. init
+    arr[start[2], start[1]] = i 
+    
+    # 2. wave expansion
+    while true
+        # println("Wave expansion #", i)
+        res = findall(x -> x == i, arr)
+        if length(res) == 0
+            break
+        end
+        for p in res
+            # println(p)
+            neighbors = [
+                [p[1]-1, p[2]],
+                [p[1]+1, p[2]],
+                [p[1], p[2]+1],
+                [p[1], p[2]-1],
+            ]
+            for n in neighbors
+                # println(n," ",arr[n[1], n[2]])
+                if arr[n[1], n[2]] == -1
+                    # println(n," ",arr[n[2], n[1]])
+                    arr[n[1], n[2]] = i + 1
+                end
+            end
+        end
+        i = i + 1
+    end
+    
+    arr = map(x -> x >= 0 ? 7 : x, arr) # oxygen
+    arr = map(x -> x == -1 ? 1 : x, arr) # visited (empty of oxygen)    
+    arr = map(x -> x == -99 ? 2 : x, arr) # walls back
+    arr = map(x -> x == -98 ? 0 : x, arr) # emptiness back
+#    draw_map(arr, [min[2],min[1]], [max[2],max[1]])
+   draw_map(arr)
+
+
+    return i-1
 end
 
-in = Channel(1)
-out = Channel(2)
 cache = Dict()
 
-@async run_intcode(deepcopy(program), in, out)
-println("Shortest path to oxygen in ", process_output(out, in), " steps")
+function map_room()
+    program = open("droid.csv") do f
+        parse.(Int, split(readlines(f)[1], ","))
+    end
+
+    in = Channel(1)
+    out = Channel(2)
+
+    @async run_intcode(deepcopy(program), in, out)
+
+    room = process_output(out, in)
+    serialize("room.jls", room)
+
+    return room
+end 
+
+room =  if isfile("room.jls")
+            deserialize("room.jls")
+        else
+            map_room()
+        end
+draw_map(room)
+target = findfirst(x -> x == 3, room)
+steps = length(route([500,500], [target[2], target[1]], room))
+println("Shortest path to oxygen in ", steps, " steps")
+steps = fill(room, [target[2], target[1]])
+println("It takes oxygen ", steps, " minutes to fill the space")
